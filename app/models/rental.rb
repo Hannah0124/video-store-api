a6ecbc6935787c2626791bde9a769636447460d8
Hannah <hannahjoo24@gmail.com>
2020-05-27 16:41:48 -0700

    Updated rental model and controller

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,7 @@
 class Rental < ApplicationRecord
+  belongs_to :customer 
+  belongs_to :video
+
+  validates :customer_id, presence: true
+  validates :video_id, presence: true
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -5,7 +5,41 @@ describe RentalsController do
   it "must get index" do 
     get rentals_path
     must_respond_with :ok
+    expect(response.header['Content-Type']).must_include 'json'
   end
 
-  expect(response.header['Content-Type]).must_include 'json' 
+  
+  describe "check_out" do 
+
+    let(:customer) {
+      customers(:pengsoo)
+    }
+    let(:video) {
+      videos(:parasite)
+    }
+
+    let(:rental_data) {
+      {
+        customer_id: customer.id,
+        video_id: video.id
+      }
+    }
+
+    it "can let a customer check out" do 
+      inventory_nums = video.available_inventory 
+      checked_out_video_nums = customer.videos_checked_out_count 
+
+      expect {
+        post check_out_path, params: rental_data
+      }.must_differ "Rental.count", 1
+
+      must_respond_with :success 
+
+      expect(response.header['Content-Type']).must_include 'json'
+
+
+      expect(Video.find(video.id).available_inventory).must_equal inventory_nums - 1
+      expect(Customer.find(customer.id).videos_checked_out_count).must_equal checked_out_video_nums + 1
+    end
+  end
 end
