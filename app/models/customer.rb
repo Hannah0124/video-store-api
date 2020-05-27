79aac6e57c51656ec1e3337f5a83a760179c27ad
Alex Robertson <arobertson335@gmail.com>
2020-05-27 14:58:36 -0700

    Adds fixture and updates model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,13 @@
 class Customer < ApplicationRecord
+
+  has_many :rentals
+
+  def as_json(options = {})
+    options[:methods] = [:videos_checkout_count]
+    super
+  end
+
+  def videos_checkout_count
+    return 0
+  end 
 end
