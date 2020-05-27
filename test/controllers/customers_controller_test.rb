656c3a338aa06ae6ce595b83f017c766de49cfcb
Alex Robertson <arobertson335@gmail.com>
2020-05-27 16:54:15 -0700

    Adds test for customer controller index method.

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -6,8 +6,33 @@ describe CustomersController do
 
   it "must get index" do 
     get customers_path
-    must_respond_with :ok
+    must_respond_with :success
+    expect(response.header["Content-Type"]).must_include "json"
   end
 
-  expect(response.header['Content-Type]).must_include 'json' 
+  it "must return all customer fields" do
+    get customers_path
+    body = JSON.parse(response.body)
+    expect(body).must_be_instance_of Array
+
+    body.each do |customer|
+      expect(customer).must_be_instance_of Hash 
+      expect(customer.keys.sort).must_equal CUSTOMER_FIELDS 
+    end
+  end
+
+  it "responds with expected type and status" do
+    get customers_path
+    must_respond_with :ok
+    expect(response.header["Content-Type"]).must_include "json"
+  end
+
+  it "returns an empty array if database has no customers" do
+    Customer.destroy_all
+    get customers_path
+    body = JSON.parse(response.body)
+    expect(body).must_be_instance_of Array
+    expect(body.length).must_equal 0
+  end
+
 end
