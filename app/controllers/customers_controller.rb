16cccd1de8932dfe8a1c54e63e07d1f574a9ab18
Hannah <hannahjoo24@gmail.com>
2020-05-27 23:28:30 -0700

    Fixed attributes to pass smoke test for rental

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,19 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all.order(:id) 
+    customers = Customer.all.order(:id)
 
     render json: customers.as_json(
-      only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), 
-      status: :ok 
-  end 
+      only: [
+        :id, 
+        :name, 
+        :registered_at, 
+        :postal_code, 
+        :phone, 
+        :videos_checked_out_count
+      ]), 
+      status: :ok
+
+      return 
+  end
 end
