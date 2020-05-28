4b4efd44e7b1d94618b96d41fe4859edfedddabc
Hannah <hannahjoo24@gmail.com>
2020-05-27 22:59:18 -0700

    Modified route to pass smoke test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :videos, only: [:index, :show, :create]
 
-  resources :rentals, only: [:index]
   post "/rentals/check-out", to: "rentals#check_out", as: "check_out"
+  
   post "/rentals/check-in", to: "rentals#check_in", as: "check_in"
 end
