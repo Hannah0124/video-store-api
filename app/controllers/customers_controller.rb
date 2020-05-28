7d87b942e636f4eb8ece6b555b5b5c774977174e
Alex Robertson <arobertson335@gmail.com>
2020-05-27 20:12:20 -0700

    Seeded database with videos and customers.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
     customers = Customer.all.order(:id) 
 
     render json: customers.as_json(
-      only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone]), 
-    ) status: :ok 
+      only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone, :videos_checked_out_count]), 
+     status: :ok
   end 
 end
