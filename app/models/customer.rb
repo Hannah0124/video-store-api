6bd3e7f4f596f6f754e518abca8271e540ca2fc2
Hannah <hannahjoo24@gmail.com>
2020-05-27 23:10:56 -0700

    Fixed a typo and added custom methods to pass smoke test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,12 +2,22 @@ class Customer < ApplicationRecord
 
   has_many :rentals
 
-  def as_json(options = {})
-    options[:methods] = [:videos_checked_out_count]
-    super
+  # def as_json(options = {})
+  #   options[:methods] = [:videos_checkout_count]
+  #   super
+  # end
+
+  # def videos_checkout_count
+  #   return 0
+  # end 
+
+  def increment_checked_out_count
+    self.videos_checked_out_count += 1
+    self.save
   end
 
-  def videos_checked_out_count
-    return 0
-  end 
+  def decrement_checked_out_count
+    self.videos_checked_out_count -= 1
+    self.save
+  end
 end
