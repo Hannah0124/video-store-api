574510cc430d083b0eae8f22949e6971d342aac6
Hannah <hannahjoo24@gmail.com>
2020-05-27 22:47:41 -0700

    Updated decrement_available_inventory & increment_available_inventory custom methods

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,14 @@ class Video < ApplicationRecord
   validates :release_date, presence: true
   validates :total_inventory, presence: true
   validates :available_inventory, presence: true
+
+  def decrement_available_inventory
+    self.available_inventory -= 1
+    self.save
+  end
+
+  def increment_available_inventory
+    self.available_inventory += 1
+    self.save
+  end
 end
