d77f9eb30cddc2277d2f290d1f11fbeff9a8edf3
Hannah <hannahjoo24@gmail.com>
2020-05-27 23:00:48 -0700

    Updated database

--- db/migrate/20200528003303_add_checkout_count_to_customers.rb
+++ db/migrate/20200528003303_add_checkout_count_to_customers.rb
@@ -1,5 +1,5 @@
 class AddCheckoutCountToCustomers < ActiveRecord::Migration[6.0]
   def change
-    add_column :customers, :video_checkout_count, :integer, default: 0
+    add_column :customers, :videos_checked_out_count, :integer, default: 0
   end
 end
--- db/migrate/20200528023630_rename_checkout_count_column.rb
+++
@@ -1,5 +0,0 @@
-class RenameCheckoutCountColumn < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :customers, :video_checkout_count, :videos_checked_out_count
-  end
-end
